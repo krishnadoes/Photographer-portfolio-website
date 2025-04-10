1c4219f70c2b4295028120438e80705d30c27928
Krishna Vishwakarma <kv29351@gmail.com>
2025-04-10 18:50:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,16 @@ module.exports = {
         secondary_on: '#74583E',
         tertiary: '#5E6572',
         tertiary_on: '#2F2F2F',
+      
+        keyframes: {
+          fadeInUp: {
+            '0%': { opacity: 0, transform: 'translateY(20px)' },
+            '100%': { opacity: 1, transform: 'translateY(0)' },
+          },
+        },
+        animation: {
+          fadeInUp: 'fadeInUp 0.6s ease-out',
+        },
       },
       fontFamily: {
         primary: 'Oxygen',
